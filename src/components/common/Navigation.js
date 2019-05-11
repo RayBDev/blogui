1120a3d687b13f252dfbf9305c38bff023727a3d
RayB <raybernard1337@gmail.com>
2019-05-11 08:02:34 -0600

    Fixed dropdown menu

--- src/components/common/Navigation.js
+++ src/components/common/Navigation.js
@@ -1,64 +1,94 @@
-import React from "react"
+import React, { Component } from "react"
 import Navbar from "react-bootstrap/Navbar"
-import NavDropdown from "react-bootstrap/NavDropdown"
 import { Link } from "gatsby"
 
-const Navigation = ({ siteTitle }) => {
-  return (
-    <Navbar expand="md" bg="dark" variant="dark" className="p-1">
-      <div className="container">
-        <Navbar.Brand href="/">{siteTitle}</Navbar.Brand>
-        <Navbar.Toggle aria-controls="basic-navbar-nav" />
-        <Navbar.Collapse id="basic-navbar-nav">
-          <ul className="navbar-nav">
-            <li className="nav-item px-2">
-              <Link to="/" activeClassName="active" className="nav-link">
-                Dashboard
-              </Link>
-            </li>
-            <li className="nav-item px-2">
-              <Link to="/posts/" activeClassName="active" className="nav-link">
-                Posts
-              </Link>
-            </li>
-            <li className="nav-item px-2">
-              <Link
-                to="/categories/"
-                activeClassName="active"
-                className="nav-link"
-              >
-                Categories
-              </Link>
-            </li>
-            <li className="nav-item px-2">
-              <Link to="/users/" activeClassName="active" className="nav-link">
-                Users
-              </Link>
-            </li>
-          </ul>
+class Navigation extends Component {
+  state = {
+    show: false,
+  }
 
-          <ul className="navbar-nav ml-auto ">
-            <NavDropdown title="&#128100; Welcome Ray" id="basic-nav-dropdown">
-              <NavDropdown.Item href="/profile/">
-                <i className="fas fa-user-circle" /> Profile
-              </NavDropdown.Item>
-              <NavDropdown.Item href="/settings/">
-                <i className="fas fa-cog" /> Settings
-              </NavDropdown.Item>
-            </NavDropdown>
-          </ul>
+  dropDownHandler = () => {
+    this.setState({ show: !this.state.show })
+  }
 
-          <ul className="navbar-nav">
-            <li className="nav-item">
-              <Link to="/login/" className="nav-link">
-                <i className="fas fa-user-times" /> Logout
-              </Link>
-            </li>
-          </ul>
-        </Navbar.Collapse>
-      </div>
-    </Navbar>
-  )
+  render() {
+    return (
+      <Navbar expand="md" bg="dark" variant="dark" className="p-1">
+        <div className="container">
+          <Navbar.Brand href="/">{this.props.siteTitle}</Navbar.Brand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <ul className="navbar-nav">
+              <li className="nav-item px-2">
+                <Link to="/" activeClassName="active" className="nav-link">
+                  Dashboard
+                </Link>
+              </li>
+              <li className="nav-item px-2">
+                <Link
+                  to="/posts/"
+                  activeClassName="active"
+                  className="nav-link"
+                >
+                  Posts
+                </Link>
+              </li>
+              <li className="nav-item px-2">
+                <Link
+                  to="/categories/"
+                  activeClassName="active"
+                  className="nav-link"
+                >
+                  Categories
+                </Link>
+              </li>
+              <li className="nav-item px-2">
+                <Link
+                  to="/users/"
+                  activeClassName="active"
+                  className="nav-link"
+                >
+                  Users
+                </Link>
+              </li>
+            </ul>
+
+            <ul className="navbar-nav ml-auto ">
+              <div className="dropdown nav-item">
+                <a
+                  href="#"
+                  class="dropdown-toggle nav-link"
+                  onClick={this.dropDownHandler}
+                >
+                  <i className="fas fa-user" /> Welcome Ray
+                </a>
+                <div
+                  className={
+                    this.state.show ? "dropdown-menu show" : "dropdown-menu"
+                  }
+                >
+                  <Link to="/profile/" className="dropdown-item">
+                    <i className="fas fa-user-circle" /> Profile
+                  </Link>
+                  <Link to="/settings/" className="dropdown-item">
+                    <i className="fas fa-cog" /> Settings
+                  </Link>
+                </div>
+              </div>
+            </ul>
+
+            <ul className="navbar-nav">
+              <li className="nav-item">
+                <Link to="/login/" className="nav-link">
+                  <i className="fas fa-user-times" /> Logout
+                </Link>
+              </li>
+            </ul>
+          </Navbar.Collapse>
+        </div>
+      </Navbar>
+    )
+  }
 }
 
 export default Navigation
